eefadea6c7b4d88111463da45c737a9d3d06c91f
Dmitriy <d.sherin@list.ru>
2026-02-27 10:40:09 +0700

    Update main

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,68 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        System.out.println("Задача 1:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Задача 2:");
+        for (int i = 10; i >= 1; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Задача 3:");
+        for (int i = 0; i <= 17; i += 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        // Задача 4: числа от 10 до -10
+        System.out.println("Задача 4:");
+        for (int i = 10; i >= -10; i--) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Задача 5:");
+        for (int year = 1904; year <= 2096; year += 4) {
+            System.out.println(year + " год является високосным");
+        }
+        System.out.println();
+
+        System.out.println("Задача 6:");
+        for (int i = 7; i <= 98; i += 7) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Задача 7:");
+        for (int i = 1; i <= 512; i *= 2) {
+            System.out.print(i + " ");
+        }
+        System.out.println("\n");
+
+        System.out.println("Задача 8:");
+        int savings = 0;
+        for (int month = 1; month <= 12; month++) {
+            savings += 29000;
+            System.out.println("Месяц " + month + ", сумма накоплений равна " + savings + " рублей");
+        }
+        System.out.println();
+
+        System.out.println("Задача 9:");
+        double savingsWithPercent = 0;
+        for (int month = 1; month <= 12; month++) {
+            savingsWithPercent = savingsWithPercent + savingsWithPercent * 0.01 + 29000;
+            System.out.printf("Месяц %d, сумма накоплений равна %.2f рублей%n", month, savingsWithPercent);
+        }
+        System.out.println();
+
+        System.out.println("Задача 10:");
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("2*" + i + "=" + (2 * i));
         }
     }
 }
